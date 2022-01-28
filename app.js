2bdc2e125d0f712b5e5952abb60d39bcfb494434
Meagan Jacobs <megpjacobs@gmail.com>
2022-01-27 19:26:28 -0800

    random fact generator added

--- app.js
+++ app.js
@@ -3,5 +3,23 @@ const text = document.querySelector('.text p');
 text.innerHTML = text.innerText.split("").map(
 (char, i) =>
 `<span style="transform:rotate(${i * 7.7}deg)">${char}</span>`
-).join("");
+).join("")
 //
+const allFacts = document.querySelector(".random-fact");//this is the html element//
+const megFacts = [ //this is the array of facts to be randomized//
+    "Thinks you\'re cool",
+    "Eats her vegetables", 
+    "Thinks omelettes are high maintenance scrambled eggs",
+    "Loves dill pickles", 
+    "Is a cat person",
+    "Likes sour candy", 
+    "Drinks a lot of coffee",
+    "Still watches cartoons",
+    "Loves learning new things!"
+]
+function randomFact() {
+    const randomIndex = Math.floor(Math.random() * megFacts.length); //this is the number in the array
+    const displayFact = megFacts[randomIndex]; //this is the random string
+    allFacts.innerText = "Meagan:" + displayFact;
+  }
+  setInterval(randomFact, 4000);
